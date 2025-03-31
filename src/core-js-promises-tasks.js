593b947b1dc3897c419b782e018db7f402bfe163
artmigalev <art_migalev@yahoo.com>
2025-03-31 20:34:04 +0300

    7

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -130,8 +130,12 @@ function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuePromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuePromises(promises) {
+  return promises.reduce((acc, promise) => {
+    return acc.then((result) =>
+      promise.then((value) => result + value.toString())
+    );
+  }, Promise.resolve(''));
 }
 
 module.exports = {
